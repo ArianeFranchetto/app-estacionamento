3c0defd4b9c651c25a1f07886c22c0d2ea83c4ed
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2023-07-04 08:10:43 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Navbar/>
+
+    <Footer/>
+
+    
   </div>
   <router-view/>
 
@@ -8,11 +12,13 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   
   }
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="footer">
+<span>
+    &#169; 2023
+</span>
+
+      
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+
+</script>
+
+<style> .footer {
+     background-color: aqua;
+     position: absolute;
+     bottom: 0;
+     width: 100%;
+     height: 2.8rem;
+     display: flex;
+     justify-content: center;
+     padding-top: 5px;
+     font-weight: 500;
+     color: #fff;
+     background-color: rgb(4, 4, 4);
+ }
+</style>
