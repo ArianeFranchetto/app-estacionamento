22d9d1573a47fd9c6df42f3b42b38e502f974039
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2023-07-05 08:05:15 -0300

    Área de Cadastro de carros

---
+++ src/components/CarroForm.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <form class="carro-form">
+            <div class="mb-3">
+                <label class="form-label">Nome:</label>
+                <input type="text" class="form-control" style="width: 300px;" id="nome" v-model="nome"
+                    placeholder="Informe o seu nome">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Nome do Carro:</label>
+                <input type="text" class="form-control" style="width: 300px;" id="nomeCarro" v-model="nomeCarro"
+                    placeholder="Informe o seu carro">
+            </div>
+            <div class="mb-3">
+
+                <label class="form-label">Horário de chegada:</label>
+                <input type="text" class="form-control" style="width: 150px;" id="horario" v-model="horario"
+                    placeholder="Informe o horário de chegada">
+                <label class="form-label">Placa:</label>
+                <input type="text" class="form-control" style="width: 150px;" id="placaCarro" v-model="placaCarro"
+                    placeholder="Informe a placa">
+            </div>
+
+            <div class="form-text">Coloque todas as letras números.</div>
+
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="concordoEmCompartilhar">
+                <label class="form-check-label">Estou certo das informações acima.</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "CarroForm"
+}
+</script>
+
+<style>
+.carro-form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    
+
+}
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="home">
-    <h1>home</h1>
-
-
-   
+    <h1>Estacionamento de Carros</h1>
+    <h3>Olá! Seja bem-vindo(a)!</h3>
+    <span>Cadastre seu carro &darr; </span>
 
   </div>
+
+<div>
+  <CarroForm/>
+</div>
+   
+
+ 
 </template>
 
 <script>
+import CarroForm from '@/components/CarroForm.vue';
 
 export default {
   name: 'HomeView',
+
+  components: {
+    CarroForm
+  }
  
 }
 </script>
+
+<style>
+.home {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 15px;
+}
+
+</style>
